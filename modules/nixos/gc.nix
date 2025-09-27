6330f8435816302df40c5bcca229b99a77008a7a
Edward Hesketh <inbox@edwardh.dev>
2025-09-28 00:11:50 +0100

    fix: do gc way more often

--- modules/nixos/gc.nix
+++ modules/nixos/gc.nix
@@ -2,8 +2,8 @@
   nix.gc = {
     automatic = true;
     persistent = false; # don't start garbage collection on boot if the last collection interval was missed.
-    dates = "monthly";
-    options = "--delete-older-than 30d"; # delete generations older than 30 days.
-    randomizedDelaySec = "3d"; # random delay to (most likely) prevent all machines from doing gc at the same time.
+    dates = "weekly";
+    options = "--delete-older-than 7d"; # delete generations older than 7 days.
+    randomizedDelaySec = "2d"; # random delay to (most likely) prevent all machines from doing gc at the same time.
   };
 }
