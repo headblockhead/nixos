4e30659cde797344fb31f2a4e93b4a5dcebc7bd3
Edward Hesketh <inbox@edwardh.dev>
2025-10-05 00:37:15 +0100

    show iso weekdate on desktop

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -54,9 +54,8 @@
               picture-uri = "file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}";
               picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
             };
-            "org/gnome/desktop/screensaver" = {
-              picture-uri = "file://${pkgs.nixos-artwork.wallpapers.nineish.gnomeFilePath}";
-              picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+            "org/gnome/desktop/calendar" = {
+              show-weekdate = true;
             };
             "org/gnome/desktop/input-sources" = {
               xkb-options = [ "terminate:ctrl_alt_bksp" "lv3:ralt_switch" "compose:rctrl" ];
