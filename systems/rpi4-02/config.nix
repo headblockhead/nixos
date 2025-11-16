e752938acac2f70560c336398da253e0eaca2b94
Edward Hesketh <inbox@edwardh.dev>
2025-11-16 23:43:32 +0000

    fix(rpi4-02): load i2c-dev

--- systems/rpi4-02/config.nix
+++ systems/rpi4-02/config.nix
@@ -2,7 +2,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 9002 8019 ];
 
-  #boot.kernelModules = [ "i2c-dev" ];
+  boot.kernelModules = [ "i2c-dev" ];
   hardware.raspberry-pi.config.all.base-dt-params = {
     i2c_arm = {
       enable = true;
