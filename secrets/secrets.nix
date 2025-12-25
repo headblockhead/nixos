99a9e4b6711e369829af0694403a1849a3cfc043
Edward Hesketh <inbox@edwardh.dev>
2025-12-25 18:48:15 +0000

    add basic kube setup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,11 +3,10 @@ let
   rpi5-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtvhxOROlavY2jNZUgpD1BkTgDNavy/TuoLnDyGWxlV";
   rpi5-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRrQrfqhA5er+AW9/wcd6Wjex79Jn+IB6YNdXfzYbTY";
   rpi5-03 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/4Qh6r7a065byYqI9gEba44DRXDuUF6vbIUduk/EJF";
-  edward-desktop-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOs2G2Yt7+A53v5tymBcbAlWnT9tLZYNSW+XGqZU6ITh";
 in
 {
   "mail-hashed-password.age".publicKeys = [ edwardh ];
   "radicale-htpasswd.age".publicKeys = [ edwardh ];
   "wg0-edwardh-key.age".publicKeys = [ edwardh ];
-  "k3s-token.age".publicKeys = [ rpi5-01 rpi5-02 rpi5-03 edward-desktop-01 ];
+  "k3s-token.age".publicKeys = [ rpi5-01 rpi5-02 rpi5-03 ];
 }
