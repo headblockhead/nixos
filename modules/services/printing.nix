a03f70c5465e76dbdcb6076a19b84d6205543299
Edward Hesketh <inbox@edwardh.dev>
2026-03-26 22:54:44 +0000

    fix: re-add printer

--- modules/services/printing.nix
+++ modules/services/printing.nix
@@ -1,3 +1,23 @@
+{ pkgs, ... }:
 {
-  services.printing.enable = true;
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "HP_Deskjet_2540";
+        location = "Upstairs Office";
+        deviceUri = "ipp://192.168.1.215/ipp/print";
+        model = "HP/hp-deskjet_2540_series.ppd.gz";
+        ppdOptions = {
+          PageSize = "A4";
+          ColorModel = "KGray";
+          OutputMode = "Draft";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "HP_Deskjet_2540";
+  };
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplip ];
+  };
 }
