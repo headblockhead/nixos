5d599a5ef7f50cf36e213cd99bfc385567a2f17e
Edward Hesketh <inbox@edwardh.dev>
2026-04-22 13:32:42 +0100

    feat: edward-dell fwupd

--- machines/edward-dell-01/default.nix
+++ machines/edward-dell-01/default.nix
@@ -38,6 +38,7 @@ inputs.nixpkgs.lib.nixosSystem {
     conf.utility
     fileSystems
     fonts.favourites
+    hardware.enableRedistributableFirmware
     networking.networkmanager
     nix.gc
     nix.registry
@@ -49,6 +50,7 @@ inputs.nixpkgs.lib.nixosSystem {
     security.rtkit
     services.desktop-managers.gnome
     services.display-managers.gdm
+    services.fwupd
     services.openssh
     services.pipewire
     users.users
