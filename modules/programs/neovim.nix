843cc22d0f79bfd3e598db5289112c6f20b0000c
Edward Hesketh <inbox@edwardh.dev>
2026-05-26 14:04:52 +0100

    fix: remove triggerCharacter setting

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -29,8 +29,6 @@
         vim.api.nvim_create_autocmd("LspAttach", { callback = function(ev)
           local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
           if client:supports_method("textDocument/completion") then 
-            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            client.server_capabilities.completionProvider.triggerCharacters = chars
             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
           end
           if not client:supports_method("textDocument/willSaveWaitUntil") and client:supports_method("textDocument/formatting") then
