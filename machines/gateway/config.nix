2eef62acf7665787100a7b6838345c95bd61accf
Edward Hesketh <inbox@edwardh.dev>
2026-07-16 01:54:57 +0100

    temporary change gateway config for EMF

--- machines/gateway/config.nix
+++ machines/gateway/config.nix
@@ -29,67 +29,22 @@
   };
 
   networking = {
-    vlans = {
-      inf-lan = {
-        id = 10;
-        interface = "ethernet4";
-      };
-      inf-iot = {
-        id = 20;
-        interface = "ethernet4";
-      };
-      inf-gst = {
-        id = 40;
-        interface = "ethernet4";
-      };
-    };
     bridges = {
-      brinf = {
-        interfaces = [ "ethernet4" ];
-      };
       brlan = {
-        interfaces = [
-          "inf-lan"
-        ];
-      };
-      briot = {
         interfaces = [
           "ethernet1"
           "ethernet2"
-          "inf-iot"
+          "ethernet3"
+          "ethernet4"
         ];
-      };
-      brsrv = {
-        interfaces = [ "ethernet3" ];
-      };
-      brgst = {
-        interfaces = [ "inf-gst" ];
       };
     };
     interfaces = {
-      # Force eths to be up (required to be added to bridges).
       ethernet1.useDHCP = false;
       ethernet2.useDHCP = false;
       ethernet3.useDHCP = false;
       ethernet4.useDHCP = false;
-
-      ethernet5 = {
-        useDHCP = true; # boring
-      };
-      brinf = {
-        ipv4.addresses = [
-          {
-            address = "172.27.1.1";
-            prefixLength = 24;
-          }
-        ];
-        ipv6.addresses = [
-          {
-            address = "fdae:a100:5f6a:1::1";
-            prefixLength = 64;
-          }
-        ];
-      };
+      ethernet5.useDHCP = true;
       brlan = {
         ipv4.addresses = [
           {
@@ -104,59 +59,12 @@
           }
         ];
       };
-      briot = {
-        ipv4.addresses = [
-          {
-            address = "172.27.20.1";
-            prefixLength = 24;
-          }
-        ];
-        ipv6.addresses = [
-          {
-            address = "fdae:a100:5f6a:20::1";
-            prefixLength = 64;
-          }
-        ];
-      };
-      brsrv = {
-        ipv4.addresses = [
-          {
-            address = "172.27.30.1";
-            prefixLength = 24;
-          }
-        ];
-        ipv6.addresses = [
-          {
-            address = "fdae:a100:5f6a:30::1";
-            prefixLength = 64;
-          }
-        ];
-      };
-      brgst = {
-        ipv4.addresses = [
-          {
-            address = "172.27.40.1";
-            prefixLength = 24;
-          }
-        ];
-        ipv6.addresses = [
-          {
-            address = "fdae:a100:5f6a:40::1";
-            prefixLength = 64;
-          }
-        ];
-      };
     };
     nat = {
       enable = true;
       enableIPv6 = true;
-      # Interfaces that can access the internet.
       internalInterfaces = [
-        "brinf"
         "brlan"
-        "briot"
-        "brsrv"
-        "brgst"
       ];
       externalInterface = "ethernet5";
     };
@@ -177,84 +85,16 @@
           allowedTCPPorts = [ ];
           allowedUDPPorts = [ ];
         };
-        brinf = {
-          allowedTCPPorts = [
-            53 # DNS
-            8080 # UniFi inform
-          ];
-          allowedUDPPorts = [
-            53 # DNS
-            67 # DHCP
-            5514 # UniFi syslog
-          ];
-        };
-        briot = {
-          allowedTCPPorts = [
-            53 # DNS
-            5060 # asterisk
-            8072 # avaya-setup
-          ];
-          allowedUDPPorts = [
-            53 # DNS
-            67 # DHCP
-            5060 # asterisk
-            5353 # mDNS
-          ];
-        };
-        brsrv = {
-          allowedTCPPorts = [
-            53 # DNS
-            5580 # matter-server
-          ];
-          allowedUDPPorts = [
-            53 # DNS
-            67 # DHCP
-            5353 # mDNS
-          ];
-        };
-        brgst = {
-          allowedTCPPorts = [
-            53 # DNS
-          ];
-          allowedUDPPorts = [
-            53 # DNS
-            67 # DHCP
-          ];
-        };
       };
-      filterForward = true;
-      extraForwardRules = ''
-        iifname brlan accept comment "from lan"
-        oifname brlan ct state established,related accept comment "returning to lan"
-
-        iifname briot oifname brsrv accept comment "from iot to srv"
-        iifname brsrv oifname briot accept comment "from srv to iot"
-      '';
     };
   };
 
   services.radvd = {
     enable = true;
     config = ''
-      interface brinf {
-        AdvSendAdvert on;
-        prefix fdae:a100:5f6a:1::/64 { };
-      };
       interface brlan {
         AdvSendAdvert on;
         prefix fdae:a100:5f6a:10::/64 { };
-      };
-      interface briot {
-        AdvSendAdvert on;
-        prefix fdae:a100:5f6a:20::/64 { };
-      };
-      interface brsrv {
-        AdvSendAdvert on;
-        prefix fdae:a100:5f6a:30::/64 { };
-      };
-      interface brgst {
-        AdvSendAdvert on;
-        prefix fdae:a100:5f6a:40::/64 { };
       };
     '';
   };
@@ -272,7 +112,6 @@
     ];
     zones."lan" = {
       master = true;
-      # Only allow the lan zone to be queried from localhost or the local network.
       allowQuery = [
         "127.0.0.0/24"
         "::1/128"
@@ -290,29 +129,12 @@
         )
 
         lan. IN NS ns.lan.
-        ns IN A 172.27.1.1
         ns IN A 172.27.10.1
-        ns IN A 172.27.20.1
-        ns IN A 172.27.30.1
-        ns IN A 172.27.40.1
 
-        gateway IN A 172.27.1.1
         gateway IN A 172.27.10.1
-        gateway IN A 172.27.20.1
-        gateway IN A 172.27.30.1
-        gateway IN A 172.27.40.1
 
-        edward-desktop-01 IN A 172.27.10.10
-
-        rpi5-01 IN A 172.27.30.51 
-        rpi5-02 IN A 172.27.30.52 
-        rpi5-03 IN A 172.27.30.53 
-        rpi4-01 IN A 172.27.30.41 
-        rpi4-02 IN A 172.27.30.42 
-
-        homeassistant IN A 172.27.20.10
-        avaya-setup IN A 172.27.20.1
-        asterisk IN A 172.27.20.1
+        avaya-setup IN A 172.27.10.1
+        asterisk IN A 172.27.10.1
       '';
     };
   };
@@ -322,11 +144,7 @@
       enable = true;
       settings = {
         interfaces-config.interfaces = [
-          "brinf"
           "brlan"
-          "briot"
-          "brsrv"
-          "brgst"
         ];
         lease-database = {
           name = "/var/lib/kea/dhcp4.leases";
@@ -343,28 +161,6 @@
         ];
         subnet4 = [
           {
-            id = 1;
-            pools = [ { pool = "172.27.1.2 - 172.27.1.254"; } ];
-            subnet = "172.27.1.0/24";
-            option-data = [
-              {
-                name = "routers";
-                data = "172.27.1.1";
-              }
-              {
-                name = "domain-name-servers";
-                data = "172.27.1.1";
-              }
-            ];
-            reservations = [
-              {
-                hostname = "u7-pro-01";
-                hw-address = "28:70:4e:8b:98:91";
-                ip-address = "172.27.1.5";
-              }
-            ];
-          }
-          {
             id = 10;
             pools = [ { pool = "172.27.10.2 - 172.27.10.254"; } ];
             subnet = "172.27.10.0/24";
@@ -380,96 +176,14 @@
             ];
             reservations = [
               {
-                hostname = "edward-desktop-01";
-                hw-address = "a0:d3:65:bb:f8:ff";
-                ip-address = "172.27.10.10";
-              }
-              {
                 hostname = "edward-laptop-01";
                 hw-address = "34:02:86:2b:84:c3";
                 ip-address = "172.27.10.11";
               }
               {
-                hostname = "edward-iphone";
-                hw-address = "5a:35:ab:18:40:82";
-                ip-address = "172.27.10.12";
-              }
-              {
-                hostname = "edward-mac-mini";
-                hw-address = "26:08:b6:4d:79:2c";
-                ip-address = "172.27.10.13";
-              }
-            ];
-          }
-          {
-            id = 20;
-            pools = [ { pool = "172.27.20.2 - 172.27.20.254"; } ];
-            subnet = "172.27.20.0/24";
-            option-data = [
-              {
-                name = "routers";
-                data = "172.27.20.1";
-              }
-              {
-                name = "domain-name-servers";
-                data = "172.27.20.1";
-              }
-            ];
-            reservations = [
-              {
-                hostname = "homeassistant";
-                hw-address = "9c:69:d3:a0:8c:7f";
-                ip-address = "172.27.20.10";
-              }
-              {
-                hostname = "hesketh-tv";
-                hw-address = "a8:13:74:17:b6:18";
-                ip-address = "172.27.20.11";
-              }
-              {
-                hostname = "scuttlebug";
-                hw-address = "4c:b9:ea:5a:4f:03";
-                ip-address = "172.27.20.12";
-              }
-              {
-                hostname = "sentinel";
-                hw-address = "4c:b9:ea:58:81:22";
-                ip-address = "172.27.20.13";
-              }
-              {
-                hostname = "ps4";
-                hw-address = "0c:fe:45:1d:e6:66";
-                ip-address = "172.27.20.14";
-              }
-              {
-                hostname = "officepi";
-                hw-address = "00:0b:81:87:e5:5f";
-                ip-address = "172.27.20.15";
-              }
-              {
-                hostname = "charlie-charger";
-                hw-address = "48:e7:29:18:6f:b0";
-                ip-address = "172.27.20.16";
-              }
-              {
-                hostname = "octo-cadlite";
-                hw-address = "30:c9:22:19:70:14";
-                ip-address = "172.27.20.17";
-              }
-              {
-                hostname = "prusa-mk4";
-                hw-address = "ec:64:c9:e9:97:9a";
-                ip-address = "172.27.20.21";
-              }
-              {
-                hostname = "panasonic-bluray";
-                hw-address = "24:78:23:01:57:b1";
-                ip-address = "172.27.20.22";
-              }
-              {
-                hostname = "edward-bedroom-phone";
+                hostname = "phone";
                 hw-address = "00:1b:4f:58:7f:cb";
-                ip-address = "172.27.20.24";
+                ip-address = "172.27.10.10";
                 option-data = [
                   {
                     name = "avaya";
@@ -477,63 +191,6 @@
                     data = "SIG=2\\,HTTPSRVR=avaya-setup.lan\\,HTTPPORT=8072";
                   }
                 ];
-              }
-            ];
-          }
-          {
-            id = 30;
-            pools = [ { pool = "172.27.30.2 - 172.27.30.254"; } ];
-            subnet = "172.27.30.0/24";
-            option-data = [
-              {
-                name = "routers";
-                data = "172.27.30.1";
-              }
-              {
-                name = "domain-name-servers";
-                data = "172.27.30.1";
-              }
-            ];
-            reservations = [
-              {
-                hostname = "rpi5-01";
-                hw-address = "2c:cf:67:94:37:82";
-                ip-address = "172.27.30.51";
-              }
-              {
-                hostname = "rpi5-02";
-                hw-address = "2c:cf:67:94:38:23";
-                ip-address = "172.27.30.52";
-              }
-              {
-                hostname = "rpi5-03";
-                hw-address = "d8:3a:dd:97:a9:c4";
-                ip-address = "172.27.30.53";
-              }
-              {
-                hostname = "rpi4-01";
-                hw-address = "dc:a6:32:31:50:3b";
-                ip-address = "172.27.30.41";
-              }
-              {
-                hostname = "rpi4-02";
-                hw-address = "e4:5f:01:11:a6:8e";
-                ip-address = "172.27.30.42";
-              }
-            ];
-          }
-          {
-            id = 40;
-            pools = [ { pool = "172.27.40.2 - 172.27.40.254"; } ];
-            subnet = "172.27.40.0/24";
-            option-data = [
-              {
-                name = "routers";
-                data = "172.27.40.1";
-              }
-              {
-                name = "domain-name-servers";
-                data = "172.27.40.1";
               }
             ];
           }
@@ -547,10 +204,10 @@
     confFiles = {
       "extensions.conf" = ''
         [from-internal]
-        exten => 1010,1,Dial(PJSIP/1010,20) ; edward-desktop-01
-        exten => 2024,1,Dial(PJSIP/2024,20) ; edward-bedroom-phone
-        exten => 2010,1,Dial(PJSIP/2010,20) ; homeassistant
-
+        exten => _X.,1,NoOp(Bridging TCP device to UDP Server)
+        same => n,Dial(PJSIP/udp-server-trunk/sip:$\{EXTEN}@sip.emf.camp:5060)
+        same => n,Hangup()
+                 
         exten => 1000,1,Answer()
         same  =>      n,Wait(2)
         same  =>      n,Playback(hello-world)
@@ -559,53 +216,45 @@
         same  =>      n,Hangup()
       '';
       "pjsip.conf" = ''
-        [transport-tcp]
-        type=transport
-        protocol=tcp
-        bind=0.0.0.0
-
         [transport-udp]
         type=transport
         protocol=udp
-        bind=0.0.0.0
+        bind=0.0.0.0:5060
 
-        [endpoint_internal](!)
+        [transport-tcp]
+        type=transport
+        protocol=tcp
+        bind=0.0.0.0:5060
+
+
+        [tcp-endpoint]
         type=endpoint
+        transport=transport-tcp
         context=from-internal
         disallow=all
         allow=g722,alaw
+        aors=tcp-endpoint
 
-        [auth_userpass](!)
+        [tcp-endpoint]
+        type=aor
+        contact=sip:172.27.10.10:5060
+
+
+        [udp-server-trunk]
+        type=endpoint
+        transport=transport-udp
+        outbound_auth=udp-server-auth
+        aors=udp-server-trunk
+
+        [udp-server-trunk]
+        type=aor
+        contact=sip:sip.emf.camp:5060
+
+        [udp-server-auth]
         type=auth
         auth_type=userpass
-
-        [aor_dynamic](!)
-        type=aor
-        max_contacts=1
-
-        [1010](endpoint_internal)
-        auth=1010
-        aors=1010
-        [1010](auth_userpass)
-        password=1010
-        username=1010
-        [1010](aor_dynamic)
-
-        [2024](endpoint_internal)
-        auth=2024
-        aors=2024
-        [2024](auth_userpass)
-        password=2024
-        username=2024
-        [2024](aor_dynamic)
-
-        [2010](endpoint_internal)
-        auth=2010
-        aors=2010
-        [2010](auth_userpass)
-        password=2010
-        username=2010
-        [2010](aor_dynamic)
+        username=TODO
+        password=TODO
       '';
     };
   };
@@ -616,7 +265,7 @@
       "avaya-setup.iot" = {
         listen = [
           {
-            addr = "172.27.20.1";
+            addr = "0.0.0.0";
             port = 8072;
           }
         ];
@@ -624,31 +273,6 @@
           root = ./avaya-http;
         };
       };
-    };
-  };
-
-  services.unifi = {
-    enable = true;
-    unifiPackage = pkgs.unifi;
-    mongodbPackage = pkgs.mongodb-7_0;
-  };
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    nssmdns6 = true;
-    domainName = "local";
-    reflector = true;
-    allowInterfaces = [
-      "brlan"
-      "briot"
-      "brsrv"
-    ];
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      userServices = true;
     };
   };
 }
